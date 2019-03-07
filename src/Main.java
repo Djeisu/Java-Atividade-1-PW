0ce20bc802bbc32bb73a1e6a613feae60d3e691f
aluno <aluno>
2019-03-07 19:30:07 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		Scanner in = new Scanner(System.in);
+		PrintStream out = System.out;
+		int option = -1;
+		
+		do {
+			System.out.println("Bem Vindo!");
+			System.out.println("1: Add User");
+			System.out.println("2: Não Tem Nada");
+			System.out.println("0: Sair");
+			
+			try {
+				option = in.nextInt();
+			}catch (Exception e){
+				System.out.println(e);
+			}
+			
+			switch (option) {
+			case 1:
+				System.out.println("\n\n Adicionou um user doidão \n");
+				continue;
+			case 2:
+				System.out.println("\n\n Aqui não tem nada ainda \n");
+				continue;
+			default:
+				System.out.println("\n\n Não tem esse valor não \n");
+				continue;
+			}
+		} while (option != 0);
+	}
+}
